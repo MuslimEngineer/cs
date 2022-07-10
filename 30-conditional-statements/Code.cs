ebaa1b4d788c63d12b34d3de453ab5637c7ad16e
kralim <109019336+kralim@users.noreply.github.com>
2022-07-10 14:36:57 +0300

    Create Code.cs
    https://www.hackerrank.com/challenges/30-conditional-statements/problem
    https://app.patika.dev/courses/csharp-101/1-if-else-yapisi-ve-ternary-if
    www.patila.dev

---
+++ 30-conditional-statements/Code.cs
@@ -0,0 +1,25 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Console.WriteLine( ( ( N%2==1) || (N>=6 && N<=20 ) ) ? "Weird" : "Not Weird"   );
+    }
+}
