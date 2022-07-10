adf6996e0d8608728396e7ed919df6f02089291e
kralim <109019336+kralim@users.noreply.github.com>
2022-07-10 17:02:40 +0300

    Create code.cs
    https://www.hackerrank.com/challenges/staircase/problem

---
+++ staircase/code.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        string son;
+        for(int i =1;i<=n;i++){
+            son="";
+        for(int j=n;j>=1;j--){
+        if(j>i){
+            son+=" ";
+        }
+        
+        else{
+            son+="#";
+        }
+        }
+        Console.WriteLine(son);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
